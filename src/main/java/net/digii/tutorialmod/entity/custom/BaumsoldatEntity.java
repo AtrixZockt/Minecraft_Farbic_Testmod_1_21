9f12e7252b65701f33688ea8425f1eb463d09988
Julian | DiGii <45559529+AtrixZockt@users.noreply.github.com>
2024-09-11 00:45:45 +0200

    Added: - Boss bar for first entity

--- src/main/java/net/digii/tutorialmod/entity/custom/BaumsoldatEntity.java
+++ src/main/java/net/digii/tutorialmod/entity/custom/BaumsoldatEntity.java
@@ -4,26 +4,39 @@ import net.digii.tutorialmod.entity.ModEntities;
 import net.minecraft.entity.AnimationState;
 import net.minecraft.entity.EntityPose;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.boss.BossBar;
+import net.minecraft.entity.boss.ServerBossBar;
+import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.recipe.Ingredient;
+import net.minecraft.registry.tag.EntityTypeTags;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
-public class BaumsoldatEntity extends AnimalEntity {
+import java.util.function.Predicate;
+
+public class BaumsoldatEntity extends HostileEntity {
+
+    private final ServerBossBar bossBar = (ServerBossBar)new ServerBossBar(this.getDisplayName(), BossBar.Color.GREEN, BossBar.Style.PROGRESS).setDarkenSky(true);
+    private static final Predicate<LivingEntity> CAN_ATTACK_PREDICATE = entity -> !entity.getType().isIn(EntityTypeTags.WITHER_FRIENDS) && entity.isMobOrPlayer();
 
     public final AnimationState idleAnimationState = new AnimationState();
     public int idleAnimationTimeout = 0;
 
-    public BaumsoldatEntity(EntityType<? extends AnimalEntity> entityType, World world) {
+    public BaumsoldatEntity(EntityType<? extends HostileEntity> entityType, World world) {
         super(entityType, world);
     }
 
@@ -54,12 +67,17 @@ public class BaumsoldatEntity extends AnimalEntity {
     protected void initGoals() {
         this.goalSelector.add(0, new SwimGoal(this));
 
-        this.goalSelector.add(1, new AnimalMateGoal(this, 1.15));
+        //this.goalSelector.add(1, new AnimalMateGoal(this, 1.15));
         this.goalSelector.add(2, new TemptGoal(this, 1.25, Ingredient.ofItems(Items.STICK), false));
         //this.goalSelector.add(3, new MeleeAttackGoal(this, 1.0, false));
         this.goalSelector.add(3, new WanderAroundFarGoal(this, 1D));
         this.goalSelector.add(3, new LookAtEntityGoal(this, PlayerEntity.class, 4f));
         this.goalSelector.add(3, new LookAroundGoal(this));
+        this.goalSelector.add(2, new MeleeAttackGoal(this, 2, false));
+
+        this.targetSelector.add(1, new RevengeGoal(this));
+        this.targetSelector.add(2, new ActiveTargetGoal<>(this, PlayerEntity.class, 0, false, false, CAN_ATTACK_PREDICATE));
+
     }
 
     // Create attributes for Entity like health
@@ -71,6 +89,7 @@ public class BaumsoldatEntity extends AnimalEntity {
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 2);
     }
 
+    /*
     @Override
     public boolean isBreedingItem(ItemStack stack) {
         return stack.isOf(Items.STICK);
@@ -79,6 +98,43 @@ public class BaumsoldatEntity extends AnimalEntity {
     @Override
     public @Nullable PassiveEntity createChild(ServerWorld world, PassiveEntity entity) {
         return ModEntities.BAUMSOLDAT.create(world);
+    }
+     */
+
+    //Boss stuff
+    @Override
+    public void readCustomDataFromNbt(NbtCompound nbt) {
+        super.readCustomDataFromNbt(nbt);
+        if (this.hasCustomName()) {
+            this.bossBar.setName(this.getDisplayName());
+        }
+    }
+
+    @Override
+    public void setCustomName(@Nullable Text name) {
+        super.setCustomName(name);
+        this.bossBar.setName(this.getDisplayName());
+    }
+
+    @Override
+    protected void mobTick() {
+        this.bossBar.setPercent(this.getHealth() / this.getMaxHealth());
+    }
+
+    public void onSummoned() {
+        this.bossBar.setPercent(1.0F);
+    }
+
+    @Override
+    public void onStartedTrackingBy(ServerPlayerEntity player) {
+        super.onStartedTrackingBy(player);
+        this.bossBar.addPlayer(player);
+    }
+
+    @Override
+    public void onStoppedTrackingBy(ServerPlayerEntity player) {
+        super.onStoppedTrackingBy(player);
+        this.bossBar.removePlayer(player);
     }
 
    // @Override
